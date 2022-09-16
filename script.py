296644245d35abe3df13ce30d867d98b775fab5c
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2022-09-15 21:30:00 -0400

    Updated repository

--- script.py
+++ script.py
@@ -36,9 +36,9 @@ def main():
 
     #sort repositories
     file = open("README.md", "w")
-    file.write('![Status](https://github.com/icaoberg/python-get-forecast/actions/workflows/main.yml/build.svg)')
-    file.write('![Status](https://github.com/icaoberg/python-get-forecast/actions/workflows/pretty.yml/badge.svg')
-    file.write('![License](https://img.shields.io/github/license/icaoberg/python-get-forecast')
+    file.write('![Status](https://github.com/icaoberg/python-get-forecast/actions/workflows/main.yml/build.svg)\n')
+    file.write('![Status](https://github.com/icaoberg/python-get-forecast/actions/workflows/pretty.yml/badge.svg)\n')
+    file.write('![License](https://img.shields.io/github/license/icaoberg/python-get-forecast)\n\n')
     file.write('# Pittsburgh Nightly Forecast\n\n')
     
     file.write(df.to_markdown(tablefmt='github'))
